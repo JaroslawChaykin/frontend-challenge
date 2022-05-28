0bda38f0e834e291c79afc53e0d22ed13a69d063
Jaroslaw <jaroslaw.chaikin@gmail.com>
2022-05-28 20:51:24 +0300

    added display of favorite cats

--- src/pages/Favorite/index.js
+++ src/pages/Favorite/index.js
@@ -1,7 +1,14 @@
+import { useSelector } from 'react-redux';
+import CatCard from '../../components/catCard/CatCard';
+import './Favorite.css'
+
 const Favorite = () => {
+    const {favorite} = useSelector(state => state.favorite)
     return (
-      <div>
-          Favorite
+      <div className={'favorite'}>
+          {
+              favorite.map(cat => <CatCard cat={cat}/>)
+          }
       </div>
     );
 };
