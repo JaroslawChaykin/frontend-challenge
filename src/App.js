94139bf6fab7df78e1afcd9769aadab791a9d254
Jaroslaw <jaroslaw.chaikin@gmail.com>
2022-05-28 18:15:53 +0300

    added navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import { BrowserRouter, Routes, Route, Navigate, NavLink } from 'react-router-dom';
+import CatsGallery from './pages/CatsGallery';
+import Favorite from './pages/Favorite';
 
 function App() {
   return (
-    <div>
-      hello
-    </div>
+    <BrowserRouter>
+        <header>
+            <NavLink to={'/'}>Все котики</NavLink>
+            <NavLink to={'/favorite'}>Любимые котики</NavLink>
+        </header>
+        <Routes>
+            <Route path={'/'} element={<CatsGallery/>}/>
+            <Route path={'/favorite'} element={<Favorite/>}/>
+            <Route path={'*'} element={<Navigate to={'/'}/>}/>
+        </Routes>
+    </BrowserRouter>
   );
 }
 
