468bbdcf93be30b8d5467c70b724dd4b11d6525f
Jaroslaw <jaroslaw.chaikin@gmail.com>
2022-05-29 13:27:23 +0300

    added connection between localstorage an store

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,38 @@
 import {configureStore, combineReducers} from '@reduxjs/toolkit';
 import CatsGallerySlice from './slices/catsGallery/catsGallerySlice';
 import FavoriteSlice from './slices/favorite/FavoriteSlice';
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['favorite']
+}
 
 const rootReducer = combineReducers({
     cats: CatsGallerySlice,
     favorite: FavoriteSlice
 })
 
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 export const store = configureStore({
-    reducer: rootReducer
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+          serializableCheck: {
+              ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+          },
+      }),
 })
+export const persistor = persistStore(store)
